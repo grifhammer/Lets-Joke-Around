278e92908dfa156865c0c18e2c313d5e7bfe696e
Griffin Hammer <grifhammer@gmail.com>
2017-10-24 15:15:09 -0700

    Minor refactor of code.

--- jokes.js
+++ jokes.js
@@ -4,10 +4,22 @@ var _ = require('lodash');
 
 var url = 'http://api.icndb.com/jokes/random/5?limitTo=[nerdy]';
 fetch(url).then(response=>{
-	response.json().then(jokeJson=>{
-		jokesList = jokeJson.value;
-		_.each(jokesList, (joke)=>{
-			console.log('- ' + joke.joke);
-		});
-	});
+	response.json().then(processJokes, handleError);
 });
+
+function printJokes(jokesList){
+	_.each(jokesList, (joke)=>{
+		console.log('- ' + joke);
+	});
+};
+
+function processJokes(jokeJson){
+	jokesList = _.map(jokeJson.value, (jokeObject)=>{
+		return jokeObject.joke;
+	});
+	printJokes(jokesList);
+};
+
+function handleError(err){
+	throw new Error(err);
+}
